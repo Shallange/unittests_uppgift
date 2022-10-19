8cebf3275bee85752af7c076282dcf685e74c364
Shallange <jimmy.kroneld@hotmail.com>
2022-10-19 14:54:00 +0200

    lade till division uppgift

---
+++ s50_funktioner/s40_matte/division/division.py
@@ -0,0 +1,8 @@
+""" skapa en funktion som heter:
+delat
+som tar två argument och delar det första argumentet med det andra argumentet
+ och returnernar resultat"""
+    
+def delat(täljare, nämnare):
+    return täljare / nämnare
+
